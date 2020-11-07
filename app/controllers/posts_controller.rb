981b10969308a0503f639b29df5d5d9b917b8201
Simon Soto <simon.tomas@hotmail.com>
2020-11-07 16:16:32 -0300

    add post with ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,12 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @post = Post.new
+
+    respond_to do |format|
+      format.html { }
+      format.js { @post }
+    end
   end
 
   # GET /posts/1
@@ -30,9 +36,11 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
+        format.js { @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js { render :index }
       end
     end
   end
